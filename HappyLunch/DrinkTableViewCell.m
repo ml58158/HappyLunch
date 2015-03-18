191982496c046f5da1eefac5ca8423f247549fa2
Matt Larkin <mattlarkin@me.com>
2015-03-18 15:01:31 -0400

    First Commit

---
+++ HappyLunch/DrinkTableViewCell.m
@@ -0,0 +1,22 @@
+//
+//  DrinkTableViewCell.m
+//  HappyLunch
+//
+//  Created by Matt Larkin on 3/18/15.
+//  Copyright (c) 2015 Matt Larkin. All rights reserved.
+//
+
+#import "DrinkTableViewCell.h"
+
+@implementation DrinkTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+-(void)setSelected:(BOOL)selected animated:(BOOL)animated {
+
+    [super setSelected:selected animated:animated];
+}
+
+@end
--- HappyLunch/ViewController.m
+++ HappyLunch/ViewController.m
@@ -7,21 +7,49 @@
 //
 
 #import "ViewController.h"
+#import "FoodTableViewCell.h"
+#import "DrinkTableViewCell.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate,foodTableViewCellDelegate, drinkTableViewCellDelegate>
+
+@property (strong, nonatomic) IBOutlet UIImageView *selectedFoodImageView;
+
+@property (strong, nonatomic) IBOutlet UIImageView *selectedDrinkImageView;
+
 
 @end
 
 @implementation ViewController
+
+NSString * const kFoodCell = @"FoodCell";
+NSString * const kDrinkCell = @"DrinkCell";
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{   if (indexPath ==0){
+    FoodTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:kFoodCell];
+    cell.delgate = self;
+    return cell;
+} else {
+    DrinkTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:kDrinkCell];
+    cell.delegate = self;
+    return cell;
+}
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 2;
+
+}
+
+-(void)foodTableViewCell:(id)cell didTapButton:(UIButton *)button {
+
+    self.selectedFoodImageView.image = button.imageView.image;
 }
 
 @end
